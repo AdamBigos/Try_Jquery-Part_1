3e63bc2098d2754e097b2443f09f1b5da52bfa8c
AdamBigos <AdamBigos@users.noreply.github.com>
2017-04-07 13:12:24 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,39 +1,40 @@
-var main = function(){
-    var leftOnesHandler = function(){
+var leftOnesHandler = function(){
         $('.highlighted').removeClass('highlighted');
         $('.left').addClass('highlighted');
-    }
+}
     
-    var rightOnesHandler = function(){
-        $('.highlighted').removeClass('highlighted');
-        $('.right').addClass('highlighted');
-    }
+var rightOnesHandler = function(){
+    $('.highlighted').removeClass('highlighted');
+    $('.right').addClass('highlighted');
+}
     
-    var mouseenterHandler = function(){
-        $(this).find('#price_top').animate({'opacity': '1'},'fast');
-    }
+var mouseenterHandler = function(){
+    $(this).find('#price_top').animate({'opacity': '1'},'fast');
+}
     
-    var mouseleaveHandler = function(){
-        $(this).find('#price_top').animate({'opacity': '0'},'fast');
-    }
+var mouseleaveHandler = function(){
+    $(this).find('#price_top').animate({'opacity': '0'},'fast');
+}
     
-    var keyupHandler = function(){
-        var instalments = +$(this).closest('.offer').data('price');
-        var monthsCounter = +$(this).val();
-        $(this).closest('.offer').find('#instalments').text(instalments / monthsCounter);
-    }
+var keyupHandler = function(){
+    var instalments = +$(this).closest('.offer').data('price');
+    var monthsCounter = +$(this).val();
+    $(this).closest('.offer').find('#instalments').text(instalments / monthsCounter);
+}
     
-    var changeHandler = function(){
-        var instalments = +$(this).closest('.offer').data('price');
-        var monthsCounter = +$(this).val();
-        $(this).closest('.offer').find('#instalments').text(instalments / monthsCounter);
-    }
+var changeHandler = function(){
+    var instalments = +$(this).closest('.offer').data('price');
+    var monthsCounter = +$(this).val();
+    $(this).closest('.offer').find('#instalments').text(instalments / monthsCounter);
+}
     
-    var detailsHandler = function(){
-        $('.highlighted').removeClass('highlighted');
-        $(this).closest('.offer').addClass('highlighted');
-        $(this).closest('.offer').find('.details').toggle();
-    }
+var detailsHandler = function(){
+    $('.highlighted').removeClass('highlighted');
+    $(this).closest('.offer').addClass('highlighted');
+    $(this).closest('.offer').find('.details').toggle();
+}
+
+var main = function(){
     
     $('.menu').on('click','.leftOnes', leftOnesHandler);
     
